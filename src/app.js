401a94e784e2f26df5624aa34a0339bebc182e32
RyanBuarque <ryanmbuarque@gmail.com>
2022-06-22 17:13:13 -0300

    routes

--- src/app.js
+++ src/app.js
@@ -1,52 +1,55 @@
 //import {openDb} from "./configDB.js";
-import {createTable, insertPessoa, updatePessoa, selectPessoas, selectPessoa, deletePessoa} from "./controler/pessoa.js";
+// import {createTable, insertPessoa, updatePessoa, selectPessoas, selectPessoa, deletePessoa} from "./controler/pessoa.js";
 
 
 import express from "express";
 const app = express();
 app.use(express.json());
 
-createTable();
+import router from "./routes.js";
+app.use(router);
 
-app.get('/', function(req, res){
-    res.send("hello world")
-});
+// createTable();
 
-app.get('/pessoas', async function(req, res){
-    let pessoas = await selectPessoas();
-    res.json(pessoas);
-});
+// app.get('/', function(req, res){
+//     res.send("hello world")
+// });
 
-app.get('/pessoa', async function(req, res){
-    let pessoa = await selectPessoa(req.body.id);
-    res.json(pessoa);
-});
+// app.get('/pessoas', async function(req, res){
+//     let pessoas = await selectPessoas();
+//     res.json(pessoas);
+// });
+
+// app.get('/pessoa', async function(req, res){
+//     let pessoa = await selectPessoa(req.body.id);
+//     res.json(pessoa);
+// });
  
-app.post('/pessoa', function(req, res){
-    insertPessoa(req.body)
-    res.json({
-        "statusCode":200 
-    })
-});
+// app.post('/pessoa', function(req, res){
+//     insertPessoa(req.body)
+//     res.json({
+//         "statusCode":200 
+//     })
+// });
 
-app.put('/pessoa', function(req, res){
-    if(req.body && !req.body.id) {
-        res.json({
-            "statusCode":400,
-            "msg":"Você precisa informar um ID"
-        })
-    } else {
-        updatePessoa(req.body)
-        res.json({
-            "statusCode":200 
-        })
-    }
-});
+// app.put('/pessoa', function(req, res){
+//     if(req.body && !req.body.id) {
+//         res.json({
+//             "statusCode":400,
+//             "msg":"Você precisa informar um ID"
+//         })
+//     } else {
+//         updatePessoa(req.body)
+//         res.json({
+//             "statusCode":200 
+//         })
+//     }
+// });
 
-app.delete('/pessoa', async function(req, res){
-    let pessoa = await deletePessoa(req.body.id);
-    res.json(pessoa);
-});
+// app.delete('/pessoa', async function(req, res){
+//     let pessoa = await deletePessoa(req.body.id);
+//     res.json(pessoa);
+// });
 
 app.listen(3000, ()=>console.log("Api Rodando"))
 
--- src/controler/pessoa.js
+++ src/controler/pessoa.js
@@ -6,31 +6,44 @@ import { openDb } from '../configDB.js';
      })
  }
 
- export async function insertPessoa(pessoa) {
+ export async function insertPessoa(req, res) {
+   let pessoa = req.body;
     openDb().then(db=>{
        db.run('INSERT INTO pessoa (nome, idade) VALUES (?,?)', [pessoa.nome, pessoa.idade]);
+    });
+    res.json({
+      "statusCode":200
     })
 }
 
- export async function updatePessoa(pessoa) {
+ export async function updatePessoa(req, res) {
+   let pessoa = req.body;
     openDb().then(db=>{
        db.run('UPDATE pessoa SET nome=?, idade=? WHERE id=?', [pessoa.nome, pessoa.idade, pessoa.id]);
+    });
+    res.json({
+      "statusCode":200
     })
 }
 
- export async function selectPessoas() {
-    return openDb().then(db=>{
-       return db.all('SELECT * FROM pessoa').then(res=>res)
+ export async function selectPessoas(req, res) {
+     openDb().then(db=>{
+        db.all('SELECT * FROM pessoa').then(pessoas=> res.json(pessoas))
     })
 }
 
- export async function selectPessoa(id) {
-    return openDb().then(db=>{
-       return db.get('SELECT * FROM pessoa WHERE id=?', [id]).then(res=>res)
+ export async function selectPessoa(req, res) {
+   let id = req.body.id;
+     openDb().then(db=>{
+        db.get('SELECT * FROM pessoa WHERE id=?', [id]).then(pessoa=> res.json(pessoa))
     })
 }
- export async function deletePessoa(id) {
-    return openDb().then(db=>{
-       return db.get('DELETE FROM pessoa WHERE id=?', [id]).then(res=>res)
+ export async function deletePessoa(req, res) {
+   let id = req.body.id;
+     openDb().then(db=>{
+        db.get('DELETE FROM pessoa WHERE id=?', [id]).then(res=>res)
+    });
+    res.json({
+      "statusCode":200
     })
 }
