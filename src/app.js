46e6e0cb66eb99b36c4e2c7d2499640e6a165c74
RyanBuarque <ryanmbuarque@gmail.com>
2022-06-22 12:39:43 -0300

    comandos controlers

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 //import {openDb} from "./configDB.js";
-import {createTable, insertPessoa} from "./controler/pessoa.js";
+import {createTable, insertPessoa, updatePessoa, selectPessoas, selectPessoa, deletePessoa} from "./controler/pessoa.js";
 
 
 import express from "express";
@@ -9,7 +9,17 @@ app.use(express.json());
 createTable();
 
 app.get('/', function(req, res){
-    res.send("hello word")
+    res.send("hello world")
+});
+
+app.get('/pessoas', async function(req, res){
+    let pessoas = await selectPessoas();
+    res.json(pessoas);
+});
+
+app.get('/pessoa', async function(req, res){
+    let pessoa = await selectPessoa(req.body.id);
+    res.json(pessoa);
 });
  
 app.post('/pessoa', function(req, res){
@@ -18,4 +28,25 @@ app.post('/pessoa', function(req, res){
         "statusCode":200 
     })
 });
+
+app.put('/pessoa', function(req, res){
+    if(req.body && !req.body.id) {
+        res.json({
+            "statusCode":400,
+            "msg":"Você precisa informar um ID"
+        })
+    } else {
+        updatePessoa(req.body)
+        res.json({
+            "statusCode":200 
+        })
+    }
+});
+
+app.delete('/pessoa', async function(req, res){
+    let pessoa = await deletePessoa(req.body.id);
+    res.json(pessoa);
+});
+
 app.listen(3000, ()=>console.log("Api Rodando"))
+
---
+++ src/routes.js
@@ -0,0 +1,3 @@
+// import { Router } from "express";
+
+// import {createTable, insertPessoa, updatePessoa, selectPessoas, selectPessoa, deletePessoa} from "./controler/pessoa.js";
